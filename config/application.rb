29a91524478537b524d3324c5065ac225092045b
Kelly Zhou <yinglinzhou0@gmail.com>
2023-03-31 14:47:39 -0400

    Start User Auth on backend

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,12 @@ module Sbotify
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore,
+      key: '_auth_me_session',
+      same_site: :lax, 
+      secure: Rails.env.production?
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
