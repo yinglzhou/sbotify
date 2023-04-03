6ae0d9a4472aca8fb1261b35128eff63608cedb3
Kelly Zhou <yinglinzhou0@gmail.com>
2023-04-03 02:07:28 -0400

    store setup frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import configureStore from './store';
+import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react';
 
-ReactDOM.render(
+const store = configureStore();
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  )
+}
+
+const renderApp = () => {ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root/>
   </React.StrictMode>,
   document.getElementById('root')
 );
+}
+
+renderApp();
