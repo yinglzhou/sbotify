d4bf2ef25a0d57a16f649c6fd7a4f29bd246d72b
Kelly Zhou <yinglinzhou0@gmail.com>
2023-04-03 00:32:30 -0400

    updated user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -15,6 +15,6 @@ class Api::UsersController < ApplicationController
   
     private
     def user_params
-      params.require(:user).permit(:email, :username, :password)
+      params.require(:user).permit(:email, :name, :password)
     end
   end
